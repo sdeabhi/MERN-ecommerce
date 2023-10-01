90172ef0ecb84cb66baa0cb7d6e2fb9ea9b7e8a3
sde Abhi <prinu331278@gmail.com>
2023-10-02 00:03:35 +0530

    backend of full stack mern exommerce

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app =require('./app')
+const express = require('express')
+
+ const port = 3000;
+
+ const server = http.createServer(app);
+
+ server.listen(port);
