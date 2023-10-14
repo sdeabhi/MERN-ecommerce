7411abc3b62e2a11ce80ef105e7e907a1f5837a3
Abhinandan Prajapati <prinu331278@gmail.com>
2023-10-14 14:56:59 +0530

    Part 1 : Product API completed.

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {createProduct, fetchAllProducts} = require('../controller/Product');
+const {createProduct, fetchAllProducts, fetchProductById, updateProduct} = require('../controller/Product');
 
 //router
 //router link added in base project
-router.post('/', createProduct).get('/', fetchAllProducts);
+router.post('/', createProduct)
+.get('/', fetchAllProducts)
+.get('/:id', fetchProductById)
+.patch('/:id', updateProduct);
 
 module.exports  = router;
